61e51fc886c26737ff257356333d2ec9242c5dd5
ManhCuong3008 <cuongnguyenmanh3008@gmail.com>
2022-01-06 10:24:44 +0700

    Seeding Data

--- eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
+++ eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -10,10 +10,10 @@ namespace eShopSolution.Data.Configurations
     public class ProductInCategoryConfiguration : IEntityTypeConfiguration<ProductInCategory>
     {
         public void Configure(EntityTypeBuilder<ProductInCategory> builder)
-        {
-            builder.HasKey(t => new {t.CategoryId, t.ProductId });
-
+        {   
             builder.ToTable("ProductInCategories");
+
+            builder.HasKey(t => new {t.CategoryId, t.ProductId });
 
             builder.HasOne(t => t.Product).WithMany(pc => pc.ProductInCategories)
                 .HasForeignKey(pc=>pc.ProductId);
